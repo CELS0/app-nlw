ea81ef8d4597df7eb5a0dfdd716fede3e20cc766
CELS0 <celsojr.300@gmail.com>
2021-10-31 09:28:53 -0300

    feat

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,11 +3,15 @@ import { View, Text, TouchableOpacity } from "react-native";
 import { styles } from "./styles";
 
 import LogoSvg from '../../assets/logo.svg';
+import { UserPhoto } from '../UserPhoto';
 
 export function Header() {
     return (
         <View style={styles.container}>
             <LogoSvg />
+
+            <UserPhoto imageUri="https://github.com/CELS0.png"/>
+
             <TouchableOpacity>
                 <Text style={styles.logoutText}>Sair</Text>
             </TouchableOpacity>
