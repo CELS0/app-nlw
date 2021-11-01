16fc9cfec70eb7858fd893c50c631d4ad4f7f08c
CELS0 <celsojr.300@gmail.com>
2021-11-01 08:49:41 -0300

    feat

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -3,6 +3,15 @@ import { ScrollView } from "react-native";
 import { api } from "../../services/api";
 import { Message, MessageProps } from "../Message";
 import { styles } from "./styles";
+import { io } from "socket.io-client";
+
+let messagesQueue: MessageProps[] = [];
+
+const socket = io(String(api.defaults.baseURL));
+socket.on("new_message", (newMessage) => {
+    messagesQueue.push(newMessage);
+});
+
 
 export function MessageList() {
     const [currentMessages, setCurrentMessages] = useState<MessageProps[]>([])
@@ -15,13 +24,26 @@ export function MessageList() {
         fectMessages()
     }, [])
 
+    useEffect(() => {
+        const timer = setInterval(() => {
+            if (messagesQueue.length > 0) {
+                setCurrentMessages((prevState) => [messagesQueue[0], prevState[0], prevState[1]]);
+                messagesQueue.shift();
+            }
+        }, 3000);
+
+        return () => clearInterval(timer);
+    }, []);
+
     return (
         <ScrollView
             style={styles.container}
             contentContainerStyle={styles.content}
             keyboardShouldPersistTaps='never'
         >
-            {currentMessages.map((message) => <Message key={message.id} data={message} />)}
+            {currentMessages.map((message) => (
+                <Message key={message.id} data={message} />
+            ))}
         </ScrollView>
     )
 }
