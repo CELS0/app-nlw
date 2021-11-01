dcd292b4ae823ef08b90c467dbda41d70097d94f
CELS0 <celsojr.300@gmail.com>
2021-11-01 08:55:15 -0300

    feat

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, KeyboardAvoidingView, Platform } from "react-native";
 import { Header } from "../../components/Header";
 import { MessageList } from "../../components/MessageList";
 import { SendMessageForm } from "../../components/SendMessageForm";
@@ -12,10 +12,15 @@ import { styles } from "./styles";
 export function Home() {
     const { user } = useAuth();
     return (
-        <View style={styles.container}>
-            <Header />
-            <MessageList />
-            {user ? <SendMessageForm /> : <SigninBox />}
-        </View>
+        <KeyboardAvoidingView
+            style={{ flex: 1 }}
+            behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        >
+            <View style={styles.container}>
+                <Header />
+                <MessageList />
+                {user ? <SendMessageForm /> : <SigninBox />}
+            </View>
+        </KeyboardAvoidingView>
     )
 }
